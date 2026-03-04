afb452bc95ce621f650ca3fa094b1817757ccbd6
xiaofeng630 <1169646434@qq.com>
2026-03-04 13:32:44 +0800

    init

---
+++ run_sft.sh
@@ -0,0 +1,38 @@
+CUDA_VISIBLE_DEVICES=1 torchrun --nproc_per_node 1 sft/supervised_finetuning.py \
+    --model_name_or_path /home/hjzd/lzz/models/Qwen2.5-7B-Instruct \
+    --train_file_dir ./data/medical/final/train \
+    --validation_file_dir ./data/medical/final/val \
+    --per_device_train_batch_size 2 \
+    --per_device_eval_batch_size 2 \
+    --do_train \
+    --do_eval \
+    --template_name qwen \
+    --use_peft True \
+    --max_train_samples 1000 \
+    --max_eval_samples 100 \
+    --model_max_length 1024 \
+    --num_train_epochs 3 \
+    --learning_rate 2e-5 \
+    --warmup_steps 50 \
+    --weight_decay 0.02 \
+    --logging_strategy steps \
+    --logging_steps 10 \
+    --eval_steps 50 \
+    --eval_strategy steps \
+    --save_steps 500 \
+    --save_strategy steps \
+    --save_total_limit 13 \
+    --gradient_accumulation_steps 32 \
+    --output_dir outputs-sft-qwen-v1 \
+    --ddp_timeout 30000 \
+    --logging_first_step True \
+    --target_modules all \
+    --lora_rank 16 \
+    --lora_alpha 32 \
+    --lora_dropout 0.05 \
+    --torch_dtype bfloat16 \
+    --bf16 \
+    --report_to tensorboard \
+    --ddp_find_unused_parameters False \
+    --gradient_checkpointing True \
+    --cache_dir ./cache --flash_attn True
